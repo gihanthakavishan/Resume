616f30463b7b4e2ef67884fa464e7504e0453ad5
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-08-12 16:38:03 +0530

    updated html and css and js

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 
-const contents = document.querySelectorAll(".content");
+// const contents = document.querySelectorAll(".content");
 
-contents.forEach((content) => {
-    content.addEventListener("click", () => {
-        content.classList.toggle("active");
+// contents.forEach((content) => {
+//     content.addEventListener("click", () => {
+//         content.classList.toggle("active");
+//     });
+// });
+
+// const contents = document.querySelectorAll(".content");
+
+// contents.forEach((content) => {
+//     content.addEventListener("click", () => {
+//         contents.forEach((otherContent) => {
+//             if (otherContent !== content) {
+//                 otherContent.classList.remove("active");
+//             }
+//         });
+//         content.classList.toggle("active");
+//     });
+// });
+
+const accordionItemHeaders = document.querySelectorAll(".title");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    // Close all open accordion items
+    accordionItemHeaders.forEach(item => {
+      if (item !== accordionItemHeader) {
+        item.classList.remove("active");
+        item.nextElementSibling.style.maxHeight = 0;
+      }
     });
+
+    // Toggle the clicked accordion item
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
 });
+
+
+
+
